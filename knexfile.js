225d7bc0339458dc9ea007201dbd975407600b9a
heloisa <heloisacmafra@gmail.com>
2023-04-11 17:52:36 -0300

    add ssl config

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,8 @@ module.exports = {
       port: Number(process.env.DB_PORT),
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE
+      database: process.env.DB_DATABASE,
+      ssl: true
     },
 
     migrations: {
